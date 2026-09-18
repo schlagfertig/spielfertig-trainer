93a3082e46285d9e1f2a11843892425cb079ae37
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-18 09:08:17 +0200

    Point logo module at four official stamp chunks

--- src/logoSrc.js
+++ src/logoSrc.js
@@ -2,6 +2,4 @@ import { P1 } from "./p1.js";
 import { P2 } from "./p2.js";
 import { P3 } from "./p3.js";
 import { P4 } from "./p4.js";
-import { P5 } from "./p5.js";
-import { P6 } from "./p6.js";
-export const LOGO_SRC = "data:image/jpeg;base64," + P1 + P2 + P3 + P4 + P5 + P6;
+export const LOGO_SRC = "data:image/jpeg;base64," + P1 + P2 + P3 + P4;
